bf1d3e7762e437def48d4f3e9322a73a6125811b
DavidCox88 <david.ryan.cox88@gmail.com>
2023-07-03 18:20:08 +0100

    searchCards update to filter all string types

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -53,8 +53,29 @@ const deleteCard = async (req, res, next) => {
 
 const searchCard = async (req, res, next) => {
   const { name } = req.body;
+  const { manaCost} = req.body;
+  const { cmc } = req.body;
+  const { type } = req.body;
+  const { oracleText } = req.body;
+  const { power } = req.body;
+  const {toughness } = req.body;
+  const { colors } = req.body;
+  const { keywords } = req.body;
+  const { setName } = req.body;
+  const { rarity } = req.body;
   try {
-    const cards = await Card.find({'name' : new RegExp(name, 'i')});
+    const cards = await Card.find({'name' : new RegExp(name, 'i'),
+                                   'manaCost':new RegExp(manaCost, 'i'),
+                                   //'cmc': cmc,
+                                   'type':new RegExp(type, 'i'),
+                                   'oracleText':new RegExp(oracleText, 'i'),
+                                   //'power':new RegExp(power, 'i'),
+                                   //'toughness':new RegExp(toughness, 'i'),
+                                   'colors':new RegExp(colors, 'i'),
+                                   'keywords':new RegExp(keywords, 'i'),
+                                   'setName':new RegExp(setName, 'i'),
+                                   'rarity':new RegExp(rarity, 'i')
+                                  });
     return res.status(200).json(cards);
   } catch (err) {
     next(err);
